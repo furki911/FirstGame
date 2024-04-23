744e85df4b377324c4fd26d1197c26af57a0b321
Furqan Masood <bhatfurqan@gmail.com>
2024-04-23 22:29:29 +0530

    adding Shadows

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -22,6 +22,11 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#72063c",
     borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    textShadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
 });
 
