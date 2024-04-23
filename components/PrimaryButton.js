3c300953e8bf383c3cc005d6e1f8715c58c40a11
Furqan Masood <bhatfurqan@gmail.com>
2024-04-23 22:29:09 +0530

    Initial Setup

---
+++ components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { Pressable, View, Text } from "react-native";
+
+const PrimaryButton = (props) => {
+  const { children } = props;
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,28 @@
+import { TextInput, View } from "react-native";
+import { StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <View>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+  },
+});
+
+export default StartGameScreen;
