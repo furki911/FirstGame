f72c2d147ae083807a8371cebd777bfb4847cf19
Furqan Masood <bhatfurqan@gmail.com>
2024-04-24 22:06:32 +0530

    boxShadowAdded

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,12 +1,36 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet } from "react-native";
 
 const PrimaryButton = (props) => {
   const { children } = props;
+  const pressHandler = () => {
+    console.log("Pressed");
+  };
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={pressHandler}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: "#4e0329",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 4,
+    elevation: 4,
+    shadowColor: "black",
+    textShadowOffset: { width: 0, height: 2 },
+    shadowRadius: 2,
+    shadowOpacity: 0.15,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
 
 export default PrimaryButton;
