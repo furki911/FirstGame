bb07ac6007a225a9a50738666080dc61b656b80c
Furqan Masood <bhatfurqan@gmail.com>
2024-04-24 23:43:00 +0530

    Background Image & Gradient

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 
+const backgroundImageSrc = require("./assets/images/background.png");
+
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StatusBar style="light" />
-      <StartGameScreen />
+      <ImageBackground
+        source={backgroundImageSrc}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StatusBar style="light" />
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: "#ddb52f",
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
