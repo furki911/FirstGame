11437145813ee62cc17d57d7caae1bb675dd0f0c
Furqan Masood <bhatfurqan@gmail.com>
2024-04-30 21:30:01 +0530

    Added and Used Fonts

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,12 +1,11 @@
 import { StyleSheet, Text } from "react-native";
-import Colors from "../../constants/colors.js";
 
 const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: "open-sans-bold",
     fontSize: 24,
-    fontWeight: "bold",
     color: "white",
     textAlign: "center",
     borderWidth: 2,
