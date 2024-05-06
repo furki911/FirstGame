79923fef5d16af3a872b3f09987b0d51fe6079a8
Furqan Masood <bhatfurqan@gmail.com>
2024-05-07 00:26:31 +0530

    Responsiveness--2 Dimensions Api Integration

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,14 +1,16 @@
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import Colors from "../../constants/colors";
 
 const Card = ({ children }) => {
   return <View style={styles.card}>{children}</View>;
 };
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   card: {
     alignItems: "center",
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 6 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
